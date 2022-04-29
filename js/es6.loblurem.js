76af6c747caf063dbc49330f349c8e0bc8429156
YilingChen <shutuzi88@gmail.com>
2022-04-29 20:11:08 +0800

    maximum errors

--- js/es6.loblurem.js
+++ js/es6.loblurem.js
@@ -11,7 +11,6 @@
 
 // Defined "self_lorem" in Global
 let self_lorem;
-// Create a ES6 Class
 class Loblurem {
   // **********Static variables********** //
   charsPerSentence = [7, 8, 9, 10, 11, 12, 13];
@@ -34,9 +33,14 @@ class Loblurem {
   comma = "。";
   // constructor
   constructor(selector) {
+    self_lorem = this;
     this.selector = selector;
-    this.generate();
-    this.onresize();
+    this.console();
+    // this.rendering();
+    // this.onresize();
+  }
+  console(){
+    // console.log(this.generateStr());
   }
   get id(){
     return "id" + Math.random().toString(16).slice(2);
@@ -48,56 +52,58 @@ class Loblurem {
       margin: 0
     }
   }
-  set styles(obj){
-    this.selector.style[obj.key] = obj.value;
+  set styles(value){
+    if(value){
+      for (var key in value) {
+        this.selector.style[key] = value[key];
+      }
+    }
   }
   get buttons(){
     return this.selector.querySelectorAll("data-loblurem-btn");
-  }
-  get hasDisplay(){
-    return this.selector.hasAttribute("data-loblurem-display");
   }
   get getDisplay(){
     return this.selector.getAttribute("data-loblurem-display");
   }
   get options(){
+    let attributes = this.selector.getAttribute("data-loblurem").split("/");
     return {
-      "counts": Number.isInteger(parseInt(this.attribute[0])) ? parseInt(this.attribute[0]) : this.attribute[0].length, // Number
-      "contents": !Number.isInteger(parseInt(this.attribute[0])) ? this.attribute[0] : this.glueWords(), // String
-      "fontSize": parseInt(this.attribute[1]), // Number
-      "lineHeight": parseInt(this.attribute[2]), // Number
-      "color": this.attribute[3], // String
-      "letterSpacing": parseInt(this.attribute[4]), // Number
-      "blur": parseInt(typeof this.attribute[5] == "undefined" ? 4 : split[5]), // Number
-      "svgWidth": this.selector.offsetWidth || this.selector.parentElement.offsetWidth,
-      "charsPerRow": Math.floor(this.options.svgWidth / (this.options.fontSize + this.options.letterSpacing)) - 1,
-      "charsLineHeight": this.options.fontSize * this.charsPerRow.length + this.options.lineHeight * (this.charsPerRow.length == 1 ? 1 : this.charsPerRow.length - 1),
-    };
+      counts: Number.isInteger(parseInt(attributes[0])) ? parseInt(attributes[0]) : attributes[0].length, // Number
+      fontSize: parseInt(attributes[1]), // Number
+      lineHeight: parseInt(attributes[2]), // Number
+      color: attributes[3], // String
+      letterSpacing: parseInt(attributes[4]), // Number
+      blur: parseInt(typeof attributes[5] == "undefined" ? 4 : attributes[5]), // Number
+    }
   }
-  set options(obj){
-    this.options[obj.key] = obj.value;
+  
+  set options(value){
+    if(value){
+      for (var key in value) {
+        this.options[key] = value[key];
+      }
+    }
   }
-  get attribute(){
-    return this.selector.getAttribute("data-loblurem").split("/");
+  get svgWidth(){
+    return this.selector.offsetWidth || this.selector.parentElement.offsetWidth;
   }
-  set attribute(value){
-    this.attribute.splice(0, 1, value);
-    this.selector.setAttribute("data-loblurem", this.attribute.join(""));
+  get charsPerRow(){
+    return Math.floor(this.svgWidth / (this.options.fontSize + this.options.letterSpacing)) - 1;
+  }
+  getContents(){
+    return !Number.isInteger(parseInt(this.options.counts)) ? this.options.counts : this.sortArrText(); // String
+  }
+  getLineHeight(){
+    return this.options.fontSize + this.options.lineHeight;
+  }
+  getSvgHeight(){
+    return (this.options.fontSize + this.options.letterSpacing) * this.sortArrText().length;
   }
   randomizeNum(a, z) {
     return Math.floor(Math.random() * (z - a + 1)) + a;
   }
   shuffleArr(arr) {
     return arr.sort(() => Math.random() - 0.5);
-  }
-  // Get the rest number to add to another number
-  // [9, 10, 12, 13, 11, 8, 7, 9, 10, 12, 13, 11, 8, 7, 9, 8]
-  spliceArr(arr, min) {
-    let theRest = arr.filter(c => c < min);
-    let theMain = arr.filter(c => c >= min);
-    let idx = theMain.indexOf(Math.min.apply(Math, theMain));
-    theRest.map(c => theMain[idx] += c);
-    return theMain;
   }
   glueArr(){
     let counts = this.options.counts;
@@ -123,7 +129,7 @@ class Loblurem {
     }
     return charsPerSentence_; // [12, 12, 12, 12, 11, 11, 10, 10, 9, 9, 8, 8, 7, 7, 7]
   }
-  glueTextArr(){
+  sortArrText(){
     let charsPerSentence = this.shuffleArr(this.glueArr());
     let marks = this.shuffleArr(this.marks);
     charsPerSentence = charsPerSentence.map(c=>this.shuffleArr(this.wordsSample).slice(0, c));
@@ -132,29 +138,29 @@ class Loblurem {
       if(i == a.length - 1) c.splice(-1, 1, this.comma);
     })
     charsPerSentence = charsPerSentence.flat();
-    charsPerSentence = charsPerSentence.map((c, i, a)=>a.slice(i * this.options.charsPerRow, i * this.options.charsPerRow + this.options.charsPerRow)).filter(c=>c.length);
+    charsPerSentence = charsPerSentence.map((c, i, a)=>a.slice(i * this.charsPerRow, i * this.charsPerRow + this.charsPerRow)).filter(c=>c.length);
     return charsPerSentence;
   }
-  
-  generateTemplate() {
+  generateStr() {
     let rowsTemplate = '';
-    let rows = this.glueTextArr();
+    let lineHeight = this.getLineHeight();
+    let svgHeight = this.getSvgHeight();
     let offsetX = this.getOffsetX();
+    let rows = this.sortArrText();
     rows.forEach((c, i, a)=>{
       rowsTemplate += `
-      <text kerning="auto" font-family="Microsoft JhengHei" font-size="${this.options.fontSize}px" x="${offsetX}px" y="${this.options.charsLineHeight*(i+1) - 2}px" letter-spacing="${this.options.letterSpacing}px" font-size="${this.options.fontSize}px" fill="${this.options.color}">${c.join("")}</text>
-      <text kerning="auto" font-family="Microsoft JhengHei" filter="url(#drop-shadow${this.id})" font-size="${this.options.fontSize}px" x="${textWidth}px" y="${this.options.charsLineHeight*(i+1) - 2}px" letter-spacing="${this.options.letterSpacing}px" textLength="${this.options.svgWidth}" font-size="${this.options.fontSize}px" fill="${this.options.color}">${c.join("")}</text>
+      <text kerning="auto" font-family="Microsoft JhengHei" font-size="${this.options.fontSize}px" x="${offsetX}px" y="${lineHeight*(i+1) - 2}px" letter-spacing="${this.options.letterSpacing}px" font-size="${this.options.fontSize}px" fill="${this.options.color}">${c.join("")}</text>
+      <text kerning="auto" font-family="Microsoft JhengHei" filter="url(#drop-shadow${this.id})" font-size="${this.options.fontSize}px" x="${offsetX}px" y="${lineHeight*(i+1) - 2}px" letter-spacing="${this.options.letterSpacing}px" textLength="${this.svgWidth}" font-size="${this.options.fontSize}px" fill="${this.options.color}">${c.join("")}</text>
       `; 
-      if(i==a.length-1){
+      if(i==(a.length-1)){
         rowsTemplate += `
-        <text kerning="auto" font-family="Microsoft JhengHei" filter="url(#drop-shadow${this.id})" font-size="${this.options.fontSize}px" x="${offsetX}px" y="${this.options.charsLineHeight*(i+1) - 2}px" letter-spacing="${this.options.letterSpacing}px" font-size="${this.options.fontSize}px" fill="${this.options.color}">${c.join("")}</text>
+        <text kerning="auto" font-family="Microsoft JhengHei" filter="url(#drop-shadow${this.id})" font-size="${this.options.fontSize}px" x="${offsetX}px" y="${lineHeight*(i+1) - 2}px" letter-spacing="${this.options.letterSpacing}px" font-size="${this.options.fontSize}px" fill="${this.options.color}">${c.join("")}</text>
         `
       }
     })
-
     return `
-    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${this.options.svgWidth}px" height="${this.options.charsLineHeight + 7}px" display="block">
-      <filter id="drop-shadow${this.id}"><feGaussianBlur stdDeviation="${(typeof this.options.blur == "undefined") ? 4 : this.options.blur}" result="drop-shadow"></feGaussianBlur></filter>
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${this.svgWidth}px" height="${svgHeight+7}px" display="block">
+      <filter id="drop-shadow${this.id}"><feGaussianBlur stdDeviation="${this.options.blur}" result="drop-shadow"></feGaussianBlur></filter>
         ${rowsTemplate}
     </svg>
     `;
@@ -163,25 +169,24 @@ class Loblurem {
   
   getOffsetX() {
     let offsetX = 3;
-    if (this.hasDisplay && this.getDisplay.length > 0) {
-      switch (this.getDisplay) {
-        case "middle":
-          offsetX = this.options.svgWidth / 2 - (this.options.fontSize * this.options.counts + this.options.letterSpacing * (this.options.counts - 1)) / 2;
-          break;
-        case "right":
-          offsetX = this.options.svgWidth - (this.options.fontSize * this.options.counts + this.options.letterSpacing * (this.options.counts - 1)) - 3;
-          break;
-        default:
-          return offsetX = 3;
-      }
-    } 
+    switch (this.getDisplay) {
+      case "middle":
+        offsetX = this.svgWidth / 2 - (this.options.fontSize * this.options.counts + this.options.letterSpacing * (this.options.counts - 1)) / 2;
+        break;
+      case "right":
+        offsetX = this.svgWidth - (this.options.fontSize * this.options.counts + this.options.letterSpacing * (this.options.counts - 1)) - 3;
+        break;
+      default:
+        offsetX = 3;
+    }
     return offsetX;
   }
   centreBtn() {
     if (!this.buttons) return;
+    let lineHeight = this.getLineHeight();
     for (let key in this.styles){this.selector.style[key] = this.styles[key]};
     buttons.forEach(c => {
-      let top = this.selector.offsetHeight - self_lorem.options.charsLineHeight / 2 - c.offsetHeight / 2 + "px" 
+      let top = this.selector.offsetHeight - lineHeight / 2 - c.offsetHeight / 2 + "px" 
       c.style.top = top;
     });
     this.selector.style.position = "relative";
@@ -189,19 +194,19 @@ class Loblurem {
   removeElement() {
     this.selector.lastElementChild.remove();
   }
-  generate() {
+  rendering() {
     this.selector.style.userSelect = "none";
-    this.selector.innerHTML += this.glueWords();
+    this.selector.innerHTML += this.generateStr();
     this.centreBtn();
   }
   onresize(){
     window.addEventListener("resize", function(){
       self_lorem.removeElement();
-      self_lorem.generate();
+      self_lorem.rendering();
     })
   }
 };
-window.addEventListener('DOMContentsLoaded', function () {
+window.addEventListener('DOMContentLoaded', function () {
   let selectors = document.querySelectorAll('[data-loblurem]');
   selectors.forEach(c=>new Loblurem(c));
 })
